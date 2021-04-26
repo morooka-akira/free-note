16b0d05b49ee524244fcd73ece6a746c021520d9
Heiya <morooka.akira.inon29@gmail.com>
2021-04-27 08:00:40 +0900

    Impl: Parser.

--- computer-system/assembler/ass/src/main.rs
+++ computer-system/assembler/ass/src/main.rs
@@ -1,6 +1,32 @@
 use ass::parser;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Problem parsing arguments: not enough arguments");
+    }
+    let config = parse_config(&args);
+
+    println!("In file {}", config.filename);
+    let mut f = File::open(config.filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text: \n{}", contents);
+
     parser::parse();
+}
+
+struct Config {
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let filename = args[1].clone();
+    return Config { filename };
 }
