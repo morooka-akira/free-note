1ede14f26be6db74056b14f5a9bcafb129043f0d
Heiya <morooka.akira.inon29@gmail.com>
2021-06-22 10:56:40 +0900

    コマンドを文字列ではなく配列を返すように変更

--- computer-system/vm/hvm/src/code_writer/mod.rs
+++ computer-system/vm/hvm/src/code_writer/mod.rs
@@ -4,15 +4,18 @@ use crate::parser::CommandType;
 pub fn write_to_file(file_name: &str, command_list: Vec<Command>) {}
 pub fn write_to_stdout(command_list: Vec<Command>) {
     for command in command_list {
-        let code = match command.command_type() {
-            CommandType::PUSH => { 
+        let code_list = match command.command_type() {
+            CommandType::PUSH => {
                 let index: i32 = command.arg2().parse().unwrap();
                 compile_push(&command.arg1(), index)
-            },
-            CommandType::ARITHMETIC => compile_add(),
-            _ => "".to_string(),
+            }
+            CommandType::ARITHMETIC => {
+                let sym = command.arg1();
+                compile_arithmetic(&sym)
+            }
+            _ => [].to_vec(),
         };
-        println!("{}", code);
+        println!("{}", code_list.join("\n"));
     }
 }
 
@@ -20,19 +23,59 @@ fn compile(command: Command) -> String {
     return "".to_string();
 }
 
-fn compile_arithmetic(command: Command) -> String {
-    return "".to_string();
-}
-
-fn compile_push(segment: &str, index: i32) -> String {
-    match segment {
-        "constant" => format!("@{}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1", index),
-        _ => "".to_string(),
+fn compile_arithmetic(symbol: &str) -> Vec<String> {
+    match symbol {
+        "add" => compile_add(),
+        "sub" => compile_sub(),
+        "neg" => compile_neg(),
+        _ => [].to_vec(),
     }
 }
 
-fn compile_add() -> String {
-    "@SP\nM=M-1\nA=M\nD=M\nA=A-1\nM=M+D".to_string()
+fn compile_push(segment: &str, index: i32) -> Vec<String> {
+    match segment {
+        "constant" => vec![
+            format!("@{}", index),
+            "D=A".to_string(),
+            "@SP".to_string(),
+            "A=M".to_string(),
+            "M=D".to_string(),
+            "@SP".to_string(),
+            "M=M+1".to_string(),
+        ],
+        _ => [].to_vec(),
+    }
+}
+
+fn compile_add<'a>() -> Vec<String> {
+    vec![
+        "@SP".to_string(),
+        "M=M-1".to_string(),
+        "A=M".to_string(),
+        "D=M".to_string(),
+        "A=A-1".to_string(),
+        "M=M+D".to_string(),
+    ]
+}
+
+fn compile_sub() -> Vec<String> {
+    vec![
+        "@SP".to_string(),
+        "M=M-1".to_string(),
+        "A=M".to_string(),
+        "D=M".to_string(),
+        "A=A-1".to_string(),
+        "M=M-D".to_string(),
+    ]
+}
+
+fn compile_neg() -> Vec<String> {
+    vec![
+        "@SP".to_string(),
+        "M=M-1".to_string(),
+        "A=M".to_string(),
+        "D=-D".to_string(),
+    ]
 }
 
 #[cfg(test)]
@@ -42,19 +85,35 @@ mod tests {
     mod compile_push {
         use super::*;
         #[test]
-        fn test_compile_push__constant() {
+        fn test_compile_push_constant() {
             assert_eq!(
                 compile_push(&"constant", 7),
-                "@7\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1"
+                ["@7", "D=A", "@SP", "A=M", "M=D", "@SP", "M=M+1"]
             );
         }
     }
 
-    mod add {
+    mod arithmetic {
         use super::*;
         #[test]
         fn test_add() {
-            assert_eq!(compile_add(), "@SP\nM=M-1\nA=M\nD=M\nA=A-1\nM=M+D");
+            assert_eq!(
+                compile_add(),
+                ["@SP", "M=M-1", "A=M", "D=M", "A=A-1", "M=M+D"]
+            );
+        }
+
+        #[test]
+        fn test_sub() {
+            assert_eq!(
+                compile_sub(),
+                ["@SP", "M=M-1", "A=M", "D=M", "A=A-1", "M=M-D"]
+            );
+        }
+
+        #[test]
+        fn test_neg() {
+            assert_eq!(compile_neg(), ["@SP", "M=M-1", "A=M", "D=-D"]);
         }
     }
 }
