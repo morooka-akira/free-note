209aad99d308b22ea4fddd127fea40e5b2779768
Heiya <morooka.akira.inon29@gmail.com>
2021-12-14 07:01:53 +0900

    impl context_switch

---
+++ rust/context_switch/src/green.rs
@@ -0,0 +1,185 @@
+use nix::sys::mman::{mprotect, ProtFlags};
+use rand;
+use std::alloc::{alloc, dealloc, Layout};
+use std::coolections::{HashMap, HashSet, LinkedList};
+use std::ffi::c_void;
+use std::ptr;
+
+extern "C" {
+    fn set_context(ctx: *mut Registers) -> u64;
+    fn switch_context(ctx: *const Registers) -> !;
+}
+
+// 構造体の内部メモリ表現がC言語と同じであることを指定
+#[repr(C)]
+struct Registers {
+    d8: u64,
+    d9: u64,
+    d10: u64,
+    d11: u64,
+    d12: u64,
+    d13: u64,
+    d14: u64,
+    d15: u64,
+    d16: u64,
+    d17: u64,
+    d18: u64,
+    d19: u64,
+    d20: u64,
+    d21: u64,
+    d22: u64,
+    d23: u64,
+    d24: u64,
+    d25: u64,
+    d26: u64,
+    d27: u64,
+    d28: u64,
+    x30: u64, // リンクレジスタ
+    sp: u64,  // スタックポインタ
+}
+
+impl Registers {
+    fn new(sp: u64) -> Self {
+        Registers {
+            d8: 0,
+            d9: 0,
+            d10: 0,
+            d11: 0,
+            d12: 0,
+            d13: 0,
+            d14: 0,
+            d15: 0,
+            d16: 0,
+            d17: 0,
+            d18: 0,
+            d19: 0,
+            d20: 0,
+            d21: 0,
+            d22: 0,
+            d23: 0,
+            d24: 0,
+            d25: 0,
+            d26: 0,
+            d27: 0,
+            d28: 0,
+            x30: entry_point as u64,
+            sp: sp,
+        }
+    }
+}
+
+type Entry = fn();
+
+// ページサイズ Linuxだと4Kb
+const PAGE_SIZE: usize = 4 * 1024; // 4Kib
+
+struct Context {
+    regs: Registers,
+    stack: *mut u8,
+    stack_layout: Layout,
+    entry: Entry,
+    id: u64,
+}
+
+impl Context {
+    fn get_regs_mut(&mut self) -> *mut Registers {
+        &mut self.regs as *mut Registers
+    }
+
+    fn get_regs(&self) -> *const Registers {
+        &self.regs as *const Registers
+    }
+
+    fn new(func: Entry, stack_size: usize, id: u64) -> Self {
+        let layout = Layout::from_size_align(stack_size, PAGE_SISE).unwrap();
+        let stack = unsafe { alloc(layout) };
+
+        // ガードページの設定
+        unsafe { mprotect(stack as *mut c_void, PAGE_SIZE, ProtFlags::PROT_NONE).unwrap() };
+        // レジスタの初期化
+        let regs = Registers::new(stack as u64 + stack_size as u64);
+        Context {
+            regs: regs,
+            stack: stack,
+            stack_layout: lyout,
+            entry: func,
+            id: id,
+        }
+    }
+}
+
+// 全てのスレッド終了時に戻ってくる先
+static mut CTX_MAIN: Option<Box<Register>> = None;
+
+// 不要なスタック領域
+static mut UNUSED_STACK: (*mut u8, Layout) = (ptr::null_mut(), Layout::new::<u8>());
+
+// スレッドの実行キュー
+static mut CONTEXTS: LinkedList<Box<Context>> = LinkedList::new();
+
+// スレッドIDの集合
+static mut ID: *mut HashSet<u64> = ptr::null_mut();
+
+fn get_id() -> u64 {
+    loop {
+        let rnd = rand::random::<u64>();
+        unsafe {
+            if !(*ID).contains(&rnd) {
+                (*ID).insert(rnd);
+                return rnd;
+            }
+        }
+    }
+}
+
+pub fn spawn(func: Entry, stack_size: usize) -> u64 {
+    unsafe {
+        let id = get_id();
+        CONTEXTS.push_back(Box::new(Context::new(func, stack_size, id)));
+        schedule();
+        id
+    }
+}
+
+pub fn schedule() {
+    unsafe {
+        if CONTEXTS.len() == 1 {
+            return;
+        }
+
+        let mut ctx = CONTEXTS.pop_front().unwrap();
+        let regs = ctx.get_regs_mut();
+        CONTEXTS.push_back(ctx);
+
+        if set_context(regs) == 0 {
+            let next = CONTEXTS.front().unwrap();
+            switch_context((**next).get_regs());
+        }
+
+        rm_unused_stack();
+    }
+}
+
+extern "C" fn entry_point() {
+    unsafe {
+        let ctx = CONTEXTS.front().unwrap();
+        ((**ctx).entry)();
+
+        let ctx = CONTEXTS.pop_front().unwrap();
+        (*ID).remove(&ctx.id);
+
+        UNUSED_STACK = ((*ctx).stack, (*ctx).stack_layout);
+
+        match CONTEXTS.front() {
+            Some(c) => {
+                switch_context((**c).get_regs());
+            }
+            None => {
+                if let Some(c) = &CTX_MAIN {
+                    switch_context(&**c as *const Registers);
+                }
+            }
+        };
+    }
+    panic("entry_point");
+}
