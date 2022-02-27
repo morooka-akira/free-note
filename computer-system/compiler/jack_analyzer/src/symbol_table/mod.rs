25ee797ba5b83fac0f8a6fc04f8decd3139d142f
Heiya <morooka.akira.inon29@gmail.com>
2022-02-28 07:16:20 +0900

    SymbolTableからScopedTableを呼び出し

--- computer-system/compiler/jack_analyzer/src/symbol_table/mod.rs
+++ computer-system/compiler/jack_analyzer/src/symbol_table/mod.rs
@@ -17,9 +17,52 @@ pub struct Symbol {
 
 #[derive(Debug, Clone)]
 pub struct SymbolTable {
-    scope_index: usize,         // どのスコープのシンボルテーブルを指しているか
-    current_table: ScopedTable, // 現在のシンボルテーブル
-    tables: Vec<ScopedTable>,   // スコープを持つシンボルテーブル
+    class_table: ScopedTable, // クラススコープを持つシンボルテーブル
+    subroutine_table: Option<ScopedTable>, // サブルーチンスコープを持つシンボルテーブル
+}
+
+impl SymbolTable {
+    fn new() -> Self {
+        Self {
+            class_table: ScopedTable::new("class".to_string()),
+            subroutine_table: None,
+        }
+    }
+
+    fn current_table(&mut self) -> &mut ScopedTable {
+        if let Some(t) = &self.subroutine_table {
+            &mut t
+        } else {
+            &mut self.class_table
+        }
+    }
+
+    fn define(&mut self, name: String, sym_type: String, kind: SymbolKind) {
+        let mut t = self.current_table();
+        t.define(name, sym_type, kind);
+        if let Some(table) = self.subroutine_table.as_ref() {
+            let mut t = *table;
+            t.define(name, sym_type, kind)
+        } else {
+            self.class_table.define(name, sym_type, kind)
+        }
+    }
+
+    fn var_count(&self, kind: SymbolKind) -> usize {
+        self.current_table().var_count(kind)
+    }
+
+    fn kind_of(&self, name: &str) -> SymbolKind {
+        self.current_table().kind_of(name)
+    }
+
+    fn type_of(&self, name: &str) -> Option<String> {
+        self.current_table().type_of(name)
+    }
+
+    fn index_of(&self, name: &str) -> Option<usize> {
+        self.current_table().index_of(name)
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -63,7 +106,6 @@ impl SymbolIndex {
 
 #[derive(Debug, Clone)]
 struct ScopedTable {
-    category: String, // var, argument, field, static, class, subroutine
     symbols: Vec<Symbol>,
     index: SymbolIndex,
 }
@@ -71,13 +113,12 @@ struct ScopedTable {
 impl ScopedTable {
     fn new(category: String) -> Self {
         ScopedTable {
-            category: category,
             symbols: Vec::new(),
             index: SymbolIndex::new(),
         }
     }
 
-    fn kind_of(&self, name: String) -> SymbolKind {
+    fn kind_of(&self, name: &str) -> SymbolKind {
         let mut kind = SymbolKind::None;
         for symbol in &self.symbols {
             if symbol.name == name {
@@ -88,7 +129,7 @@ impl ScopedTable {
         kind
     }
 
-    fn define(&mut self, name: String, sym_type: String, kind: SymbolKind) -> usize {
+    fn define(&mut self, name: String, sym_type: String, kind: SymbolKind) {
         let index = self.index.current(&kind);
         self.index.advance(&kind);
         let symbol = Symbol {
@@ -98,7 +139,6 @@ impl ScopedTable {
             kind: kind,
         };
         self.symbols.push(symbol);
-        index
     }
 
     fn var_count(&self, kind: SymbolKind) -> usize {
@@ -111,7 +151,7 @@ impl ScopedTable {
         }
     }
 
-    fn type_of(&self, name: String) -> Option<String> {
+    fn type_of(&self, name: &str) -> Option<String> {
         let mut sym_type: Option<String> = None;
         for symbol in &self.symbols {
             if symbol.name == name {
@@ -121,7 +161,7 @@ impl ScopedTable {
         sym_type
     }
 
-    fn index_of(&self, name: String) -> Option<usize> {
+    fn index_of(&self, name: &str) -> Option<usize> {
         let mut index: Option<usize> = None;
         for symbol in &self.symbols {
             if symbol.name == name {
@@ -144,9 +184,9 @@ mod tests {
             let mut table = ScopedTable::new("class".to_string());
             table.define("x".to_string(), "int".to_string(), SymbolKind::Field);
             table.define("y".to_string(), "String".to_string(), SymbolKind::Static);
-            assert_eq!(table.kind_of("a".to_string()), SymbolKind::None);
-            assert_eq!(table.kind_of("x".to_string()), SymbolKind::Field);
-            assert_eq!(table.kind_of("y".to_string()), SymbolKind::Static);
+            assert_eq!(table.kind_of("a"), SymbolKind::None);
+            assert_eq!(table.kind_of("x"), SymbolKind::Field);
+            assert_eq!(table.kind_of("y"), SymbolKind::Static);
         }
 
         #[test]
@@ -176,9 +216,9 @@ mod tests {
             let mut table = ScopedTable::new("class".to_string());
             table.define("x".to_string(), "int".to_string(), SymbolKind::Field);
             table.define("y".to_string(), "String".to_string(), SymbolKind::Static);
-            assert_eq!(table.type_of("x".to_string()).unwrap(), "int");
-            assert_eq!(table.type_of("y".to_string()).unwrap(), "String");
-            assert_eq!(table.type_of("unknown".to_string()), None);
+            assert_eq!(table.type_of("x").unwrap(), "int");
+            assert_eq!(table.type_of("y").unwrap(), "String");
+            assert_eq!(table.type_of("unknown"), None);
         }
 
         #[test]
@@ -186,9 +226,9 @@ mod tests {
             let mut table = ScopedTable::new("class".to_string());
             table.define("x".to_string(), "int".to_string(), SymbolKind::Field);
             table.define("y".to_string(), "String".to_string(), SymbolKind::Field);
-            assert_eq!(table.index_of("x".to_string()).unwrap(), 0);
-            assert_eq!(table.index_of("y".to_string()).unwrap(), 1);
-            assert_eq!(table.index_of("unknown".to_string()), None);
+            assert_eq!(table.index_of("x").unwrap(), 0);
+            assert_eq!(table.index_of("y").unwrap(), 1);
+            assert_eq!(table.index_of("unknown"), None);
         }
     }
 }
