8b904eb3d93cd64bcbacb8c1be5d0b8003a2f482
Heiya <morooka.akira.inon29@gmail.com>
2022-05-03 11:55:06 +0900

    2.4 letの解析done

--- interpreter-book/monkey/src/ast.rs
+++ interpreter-book/monkey/src/ast.rs
@@ -1,52 +1,82 @@
+use std::rc::Rc;
+
+use downcast_rs::{impl_downcast, Downcast};
+
 use crate::token::Token;
 
-trait Node {
+pub trait Node: Downcast {
     fn token_literal(&self) -> String;
 }
+impl_downcast!(Node);
 
-trait Statement: Node {
+pub trait Statement: Node {
     fn statement_node(&self) -> bool;
 }
+impl_downcast!(Statement);
 
-trait Expression: Node {
+pub trait Expression: Node {
     fn expression_node(&self) -> bool;
 }
 
-struct Identifier {
-    token: Token,
-    value: String,
-}
-
-struct LetStatement {
-    token: Token,
-    name: Identifier,
-    value: Box<dyn Expression>,
-}
 pub struct Program {
-    statements: Vec<Box<dyn Statement>>,
+    pub statements: Vec<Box<dyn Statement>>,
 }
 
-impl Identifier {
-    fn expression_node(&self) {
-        println!("Identifier statement_node");
+use core::fmt::Debug;
+impl Debug for dyn Statement {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let r = write!(f, "Statement -> {}", self.token_literal());
+        r
     }
+}
 
-    fn token_literale(&self) -> String {
+#[derive(Debug)]
+pub struct Identifier {
+    pub token: Rc<Token>,
+    pub value: String,
+}
+
+impl Expression for Identifier {
+    fn expression_node(&self) -> bool {
+        println!("Identifier statement_node");
+        true
+    }
+}
+
+impl Node for Identifier {
+    fn token_literal(&self) -> String {
         self.token.literal.to_string()
     }
 }
 
-impl LetStatement {
-    fn statement_node(&self) {
-        println!("let statement statement_node");
-    }
+#[derive(Debug)]
+pub struct LetStatement {
+    pub token: Rc<Token>,
+    pub name: Identifier,
+    // TODO: 後で実装
+    // pub value: Box<dyn Expression>,
+}
 
+impl Statement for LetStatement {
+    fn statement_node(&self) -> bool {
+        println!("let statement statement_node");
+        true
+    }
+}
+
+impl Node for LetStatement {
     fn token_literal(&self) -> String {
         self.token.literal.to_string()
     }
 }
 
 impl Program {
+    pub fn new() -> Program {
+        Program {
+            statements: Vec::new(),
+        }
+    }
+
     fn token_literal(&self) -> String {
         if self.statements.len() > 0 {
             self.statements[0].token_literal()
--- interpreter-book/monkey/src/parser.rs
+++ interpreter-book/monkey/src/parser.rs
@@ -1,11 +1,16 @@
 use std::rc::Rc;
 
-use crate::{ast::Program, lexer::Lexer, token::Token};
+use crate::{
+    ast::{Identifier, LetStatement, Program, Statement},
+    lexer::Lexer,
+    token::{Token, TokenType, ASSIGN, EOF, IDENT, LET, SEMICOLON},
+};
 
 struct Parser<'a> {
     lexer: &'a mut Lexer<'a>,
     cur_token: Rc<Token>,
     peek_token: Rc<Token>,
+    errors: Vec<String>,
 }
 
 impl<'a> Parser<'a> {
@@ -16,6 +21,7 @@ impl<'a> Parser<'a> {
             lexer,
             cur_token,
             peek_token,
+            errors: Vec::new(),
         }
     }
 
@@ -25,6 +31,139 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_program(&mut self) -> Result<Program, String> {
-        Err(String::from("Nothing"))
+        let mut program = Program::new();
+
+        while !self.cur_token_is(EOF) {
+            let stmt = self.parse_statement();
+            match stmt {
+                Some(stmt) => program.statements.push(stmt),
+                None => println!("Statement not found"),
+            }
+            self.next_token();
+        }
+        Ok(program)
+    }
+
+    fn parse_statement(&mut self) -> Option<Box<dyn Statement>> {
+        println!("{}", self.cur_token.token_type);
+        match self.cur_token.token_type {
+            LET => self.parse_let_statement(),
+            _ => {
+                println!("token_type unknown");
+                None
+            }
+        }
+    }
+
+    fn parse_let_statement(&mut self) -> Option<Box<dyn Statement>> {
+        let token = Rc::clone(&self.cur_token);
+
+        if !self.expect_peek(IDENT) {
+            return None;
+        }
+
+        let name = Identifier {
+            token: Rc::clone(&self.cur_token),
+            value: self.cur_token.literal.clone(),
+        };
+
+        if !self.expect_peek(ASSIGN) {
+            return None;
+        }
+        // TODO: とりあえずセミコロンまで値を読み飛ばす
+        while !self.cur_token_is(SEMICOLON) {
+            self.next_token();
+        }
+        Some(Box::new(LetStatement { token, name }))
+    }
+
+    fn expect_peek(&mut self, token_type: TokenType) -> bool {
+        if self.peek_token_is(token_type) {
+            self.next_token();
+            true
+        } else {
+            self.peek_error(token_type);
+            false
+        }
+    }
+
+    fn cur_token_is(&mut self, token_type: TokenType) -> bool {
+        self.cur_token.token_type == token_type
+    }
+
+    fn peek_token_is(&mut self, token_type: TokenType) -> bool {
+        self.peek_token.token_type == token_type
+    }
+
+    fn errors(&self) -> &Vec<String> {
+        &self.errors
+    }
+
+    fn peek_error(&mut self, token_type: TokenType) {
+        let msg = format!(
+            "expected next token to be {}, got {} instead",
+            token_type, self.peek_token.token_type
+        );
+        self.errors.push(msg);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        ast::{LetStatement, Node},
+        lexer::Lexer,
+    };
+
+    #[test]
+    fn test_let_statement() {
+        let input = r#"
+            let x = 5;
+            let y = 10;
+            let foobar = 838383;
+        "#;
+
+        let mut l = Lexer::new(input);
+        let mut p = super::Parser::new(&mut l);
+
+        let program = p.parse_program();
+        if program.is_err() {
+            panic!("parse_program() returned an error");
+        }
+        check_parser_errors(&p);
+        let program = program.unwrap();
+        let statement_len = program.statements.len();
+        if statement_len != 3 {
+            panic!(
+                "program.statements does not contain 3 statements. got={}",
+                statement_len
+            );
+        }
+        let expected_identifier = vec!["x", "y", "foobar"];
+        for (i, expected) in expected_identifier.iter().enumerate().take(statement_len) {
+            let st = &program.statements[i];
+            let let_st = (*st).downcast_ref::<LetStatement>().unwrap();
+            // TODO: 値のテストは後で書く
+            assert_eq!(
+                let_st.name.value,
+                expected.to_string(),
+                "let_st.name.value not correct"
+            );
+            assert_eq!(
+                let_st.name.token_literal(),
+                expected.to_string(),
+                "let_st.name.token_literal not correct"
+            );
+        }
+    }
+
+    fn check_parser_errors(p: &super::Parser) {
+        if p.errors().is_empty() {
+            return;
+        }
+        for error in p.errors() {
+            print!("error -> {}", error);
+        }
+        panic!("parser has {} errors", p.errors().len());
     }
 }
