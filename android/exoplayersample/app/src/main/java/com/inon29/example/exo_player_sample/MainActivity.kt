7e2fbacfab1fbb2e91e661d44ee54fb584448d14
Heiya <morooka.akira.inon29@gmail.com>
2022-06-01 16:28:50 +0900

    ExoPlayerの再生サンプルプロジェクトを追加

---
+++ android/exoplayersample/app/src/main/java/com/inon29/example/exo_player_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.inon29.example.exo_player_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
