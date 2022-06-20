e41791e7d9768970bc29e057bc35861b0999acda
Heiya <morooka.akira.inon29@gmail.com>
2022-06-21 07:50:15 +0900

    riverpodのサンプルを動かすようにした

---
+++ flutter/riverpod-sample/lib/main.dart
@@ -0,0 +1,254 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_sample/todo.dart';
+
+final addTodoKey = UniqueKey();
+final activeFilterKey = UniqueKey();
+final completedFilterKey = UniqueKey();
+final allFilterKey = UniqueKey();
+
+final todoListProvider = StateNotifierProvider<TodoList, List<Todo>>((ref) {
+  return TodoList(const [
+    Todo(id: 'todo-0', description: 'hi'),
+    Todo(id: 'todo-1', description: 'hello'),
+    Todo(id: 'todo-2', description: 'bonjour'),
+  ]);
+});
+
+enum TodoListFilter {
+  all,
+  active,
+  completed,
+}
+
+final todoListFilter = StateProvider((_) => TodoListFilter.all);
+
+final uncompletedTodosCount = Provider<int>((ref) {
+  return ref.watch(todoListProvider).where((todo) => !todo.completed).length;
+});
+
+final filteredTodos = Provider<List<Todo>>((ref) {
+  final filter = ref.watch(todoListFilter);
+  final todos = ref.watch(todoListProvider);
+
+  switch (filter) {
+    case TodoListFilter.all:
+      return todos;
+    case TodoListFilter.active:
+      return todos.where((todo) => !todo.completed).toList();
+    case TodoListFilter.completed:
+      return todos.where((todo) => todo.completed).toList();
+  }
+});
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+final counterProvider = StateProvider((ref) => 0);
+
+class Home extends HookConsumerWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todos = ref.watch(filteredTodos);
+    final newTodoController = useTextEditingController();
+
+    return GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+            body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          children: [
+            const Title(),
+            TextField(
+              key: addTodoKey,
+              controller: newTodoController,
+              decoration:
+                  const InputDecoration(labelText: 'What needs to be done?'),
+              onSubmitted: (value) {
+                ref.read(todoListProvider.notifier).add(value);
+                newTodoController.clear();
+              },
+            ),
+            const SizedBox(height: 42),
+            const Toolbar(),
+            if (todos.isNotEmpty) const Divider(height: 0),
+            for (var i = 0; i < todos.length; i++) ...[
+              if (i > 0) const Divider(height: 0),
+              Dismissible(
+                key: ValueKey(todos[i].id),
+                child: ProviderScope(
+                  overrides: [
+                    _currentTodo.overrideWithValue(todos[i]),
+                  ],
+                  child: const TodoItem(),
+                ),
+              )
+            ]
+          ],
+        )));
+  }
+}
+
+class Toolbar extends HookConsumerWidget {
+  const Toolbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final filter = ref.watch(todoListFilter);
+
+    Color? textColorFor(TodoListFilter value) {
+      return filter == value ? Colors.blue : Colors.black;
+    }
+
+    return Material(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+              child: Text(
+            '${ref.watch(uncompletedTodosCount).toString()} items left',
+            overflow: TextOverflow.ellipsis,
+          )),
+          Tooltip(
+              key: allFilterKey,
+              message: 'All todos',
+              child: TextButton(
+                onPressed: () => ref.read(todoListFilter.notifier).state =
+                    TodoListFilter.all,
+                style: ButtonStyle(
+                    visualDensity: VisualDensity.compact,
+                    foregroundColor: MaterialStateProperty.all(
+                        textColorFor(TodoListFilter.all))),
+                child: const Text('All'),
+              )),
+          Tooltip(
+              key: activeFilterKey,
+              message: 'Only uncompleted todos',
+              child: TextButton(
+                onPressed: () => ref.read(todoListFilter.notifier).state =
+                    TodoListFilter.active,
+                style: ButtonStyle(
+                    visualDensity: VisualDensity.compact,
+                    foregroundColor: MaterialStateProperty.all(
+                        textColorFor(TodoListFilter.active))),
+                child: const Text('Active'),
+              )),
+          Tooltip(
+              key: completedFilterKey,
+              message: 'Only completed todos',
+              child: TextButton(
+                onPressed: () => ref.read(todoListFilter.notifier).state =
+                    TodoListFilter.completed,
+                style: ButtonStyle(
+                    visualDensity: VisualDensity.compact,
+                    foregroundColor: MaterialStateProperty.all(
+                        textColorFor(TodoListFilter.completed))),
+                child: const Text('Completed'),
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("todos",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            color: Color.fromARGB(38, 47, 47, 247),
+            fontSize: 100,
+            fontWeight: FontWeight.w100,
+            fontFamily: 'Helvetiva Neue'));
+  }
+}
+
+final _currentTodo = Provider<Todo>((ref) => throw UnimplementedError());
+
+class TodoItem extends HookConsumerWidget {
+  const TodoItem({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todo = ref.watch(_currentTodo);
+    final itemFocusNode = useFocusNode();
+    final itemIsFocused = useIsFocused(itemFocusNode);
+
+    final textEditingController = useTextEditingController();
+    final textFieldFocusNode = useFocusNode();
+
+    return Material(
+      color: Colors.white,
+      elevation: 6,
+      child: Focus(
+        focusNode: itemFocusNode,
+        onFocusChange: (focused) {
+          if (focused) {
+            textEditingController.text = todo.description;
+          } else {
+            ref
+                .read(todoListProvider.notifier)
+                .edit(id: todo.id, description: textEditingController.text);
+          }
+        },
+        child: ListTile(
+          onTap: () {
+            itemFocusNode.requestFocus();
+            textFieldFocusNode.requestFocus();
+          },
+          leading: Checkbox(
+              value: todo.completed,
+              onChanged: (value) {
+                ref.read(todoListProvider.notifier).toggle(todo.id);
+              }),
+          title: itemIsFocused
+              ? TextField(
+                  autofocus: true,
+                  focusNode: textFieldFocusNode,
+                  controller: textEditingController,
+                )
+              : Text(todo.description),
+        ),
+      ),
+    );
+  }
+}
+
+bool useIsFocused(FocusNode node) {
+  final isFocused = useState(node.hasFocus);
+
+  useEffect(() {
+    void listener() {
+      isFocused.value = node.hasFocus;
+    }
+
+    node.addListener(listener);
+    return () => node.removeListener(listener);
+  }, [node]);
+  return isFocused.value;
+}
