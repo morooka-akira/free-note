fa74d1e3bae66d6fdc274e6e9f22adace895a8a2
Heiya <morooka.akira.inon29@gmail.com>
2024-03-10 10:31:35 +0900

    rename

--- js/deno/deno-react/build.ts
+++ js/deno/deno-react/build.ts
@@ -1,6 +1,6 @@
 import { bundle } from "https://deno.land/x/emit@0.38.2/mod.ts";
 
-const url = new URL("./main.tsx", import.meta.url);
+const url = new URL("./index.tsx", import.meta.url);
 const result = await bundle(url, {
   minify: false,
 });
